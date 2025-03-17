c3b4742adff0044e7b688ff64bab55c2badc7403
tyrrell-IH <gotothertm@gmail.com>
2025-03-17 12:40:43 +0900

    add CommentArt class

--- bin/cuteMispronunciation.js
+++ bin/cuteMispronunciation.js
@@ -4,5 +4,4 @@ import { Response } from "../lib/response.js";
 const ui = new CLI();
 const word = await ui.inputWord();
 const response = new Response(word);
-const responseWord = response.createWord();
-console.log(responseWord);
+response.exec();
