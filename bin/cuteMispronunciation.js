379f38145bbd4150a96b7631d009a650a69d5ba5
tyrrell-IH <gotothertm@gmail.com>
2025-03-18 21:03:44 +0900

    fixed to abort processing when ctrl + C entered

--- bin/cuteMispronunciation.js
+++ bin/cuteMispronunciation.js
@@ -2,6 +2,15 @@ import { CLI } from "../lib/cli.js";
 import { Comment } from "../lib/comment.js";
 
 const ui = new CLI();
-const word = await ui.inputWord();
+let word;
+try {
+  word = await ui.inputWord();
+} catch (error) {
+  if (error === "") {
+    process.exit(1);
+  } else {
+    throw error;
+  }
+}
 const comment = new Comment(word);
 comment.exec();
